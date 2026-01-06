0d26f03c3a028600a0dd6c900348bea6a633c3ee
azeem-cyber <azeemgolden8@gmail.com>
2026-01-06 12:09:24 +0530

    Update calculator with a modern UI

--- script.js
+++ script.js
@@ -1,27 +1,119 @@
-let result = document.getElementById('result');
-let operator = '';
-let operand1 = '';
-let operand2 = '';
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
+    }
 
-function appendNumber(number) {
-  result.value += number;
+    clear() {
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
+        this.updateDisplay()
+    }
+
+    delete() {
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
+        this.updateDisplay()
+    }
+
+    appendNumber(number) {
+        if (number === '.' && this.currentOperand.includes('.')) return
+        this.currentOperand = this.currentOperand.toString() + number.toString()
+        this.updateDisplay()
+    }
+
+    chooseOperation(operation) {
+        if (this.currentOperand === '') return
+        if (this.previousOperand !== '') {
+            this.compute()
+        }
+        this.operation = operation
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ''
+        this.updateDisplay()
+    }
+
+    compute() {
+        let computation
+        const prev = parseFloat(this.previousOperand)
+        const current = parseFloat(this.currentOperand)
+        if (isNaN(prev) || isNaN(current)) return
+        switch (this.operation) {
+            case '+':
+                computation = prev + current
+                break
+            case '-':
+                computation = prev - current
+                break
+            case '×':
+                computation = prev * current
+                break
+            case '÷':
+                computation = prev / current
+                break
+            default:
+                return
+        }
+        this.currentOperand = computation
+        this.operation = undefined
+        this.previousOperand = ''
+        this.updateDisplay()
+    }
+
+    getDisplayNumber(number) {
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDisplay
+        if (isNaN(integerDigits)) {
+            integerDisplay = ''
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
+        }
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`
+        } else {
+            return integerDisplay
+        }
+    }
+
+    updateDisplay() {
+        this.currentOperandTextElement.innerText = this.currentOperand
+        if (this.operation != null) {
+            this.previousOperandTextElement.innerText = `${this.previousOperand} ${this.operation}`
+        } else {
+            this.previousOperandTextElement.innerText = ''
+        }
+    }
 }
 
-function setOperator(op) {
-  operator = op;
-  operand1 = result.value;
-  result.value = '';
+const previousOperandTextElement = document.getElementById('previous-operand')
+const currentOperandTextElement = azeem-cyber.getElementById('current-operand')
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+function appendNumber(number) {
+    calculator.appendNumber(number)
+    calculator.updateDisplay()
+}
+
+function setOperator(operator) {
+    calculator.chooseOperation(operator)
+    calculator.updateDisplay()
 }
 
 function calculate() {
-  operand2 = result.value;
-  let res = eval(operand1 + operator + operand2);
-  result.value = res;
+    calculator.compute()
+    calculator.updateDisplay()
 }
 
 function clearResult() {
-  result.value = '';
-  operator = '';
-  operand1 = '';
-  operand2 = '';
+    calculator.clear()
+    calculator.updateDisplay()
+}
+
+function deleteLast() {
+    calculator.delete()
+    calculator.updateDisplay()
 }
