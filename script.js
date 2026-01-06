d274cb2adcf620a6b3e25ea208f85b46c1bf8382
azeem-cyber <azeemgolden8@gmail.com>
2026-01-06 12:15:32 +0530

    feat: Add tabs and heading for a more immersive user experience

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+function openTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tab-content");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tab-button");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
 class Calculator {
     constructor(previousOperandTextElement, currentOperandTextElement) {
         this.previousOperandTextElement = previousOperandTextElement
@@ -121,3 +135,5 @@ function clearResult() {
 function deleteLast() {
     calculator.delete()
 }
+// Get the element with id="defaultOpen" and click on it
+document.getElementsByClassName("tab-button")[0].click();
